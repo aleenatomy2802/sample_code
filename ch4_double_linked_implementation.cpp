eafd86cce329d20ddc8eb1151ea288ff4567e68d
truc.huynh.civ@swf.army.mil <truc.huynh.civ@swf.army.mil>
2024-02-02 15:42:23 -0600

    add the definition for the add functions

--- ch4_double_linked_implementation.cpp
+++ ch4_double_linked_implementation.cpp
@@ -7,6 +7,7 @@ public:
     Node* next;
 
     Node(int value) : data(value), prev(nullptr), next(nullptr) {}
+    // Constructor that set default value
 };
 
 class DoublyLinkedList {
@@ -17,6 +18,21 @@ private:
 public:
     DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
+    /*
+    Node* newNode = new Node(data);: This line creates a new node with the given data value. 
+    The newNode is a pointer to the newly allocated node in the memory.
+
+    if (!head) { ... } else { ... }: 
+    This conditional statement checks whether the list is empty or not.
+
+    If the list is empty (if (!head)), it means there are no nodes in the list. In this case, the function sets both the head and tail pointers to the new node (newNode). This is because, when the list is empty, the new node becomes both the first and the last node in the list.
+
+    If the list is not empty (else part), it means there are already nodes in the list. In this case:
+
+    newNode->next = head;: The next pointer of the new node is set to point to the current head of the list. This step links the new node to the existing list.
+    head->prev = newNode;: The prev pointer of the current head of the list is set to point back to the new node. This step establishes the backward link from the existing head to the new node.
+    head = newNode;: The head pointer is updated to point to the new node, making the new node the new head of the list.
+    */
     void insertAtBeginning(int data) {
         Node* newNode = new Node(data);
         if (!head) {
