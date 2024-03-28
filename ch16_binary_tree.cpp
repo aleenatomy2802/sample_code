199f6d6ff3c64183f0e6ce666b1f948bc3ca7b69
truc.huynh.civ@swf.army.mil <truc.huynh.civ@swf.army.mil>
2024-03-28 17:08:07 -0500

    update binary tree

--- ch16_binary_tree.cpp
+++ ch16_binary_tree.cpp
@@ -119,26 +119,26 @@ private:
     }
 
     void printLevelOrder(TreeNode* root) {
-    if (!root) return;
+        if (!root) return;
 
-    queue<TreeNode*> q;
-    q.push(root);
+        queue<TreeNode*> q;
+        q.push(root);
 
-    while (!q.empty()) {
-        int levelSize = q.size();
+        while (!q.empty()) {
+            int levelSize = q.size();
 
-        for (int i = 0; i < levelSize; i++) {
-            TreeNode* node = q.front();
-            q.pop();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
 
-            cout << node->value << " ";
+                cout << node->value << " ";
 
-            if (node->left) q.push(node->left);
-            if (node->right) q.push(node->right);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            cout << endl; // New line for each level
         }
-        cout << endl; // New line for each level
     }
-}
 
 public:
     BinarySearchTree() : root(nullptr) {}
